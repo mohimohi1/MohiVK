3c309536fe1fc87fac0b3fc477ae910c0d27235d
mohimohi1 <mo.misha@bk.ru>
2019-07-04 21:33:29 +0300

    1

---
+++ MohiVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MohiVK
+//
+//  Created by Михаил Мохнач on 30/06/2019.
+//  Copyright © 2019 Михаил Мохнач. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
